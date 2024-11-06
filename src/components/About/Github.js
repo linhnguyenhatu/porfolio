0d0ab0bbbacd4d2e01d059afedba2db2782d4d1c
Tu Linh H Nguyen <linhnguyenhatu@gmail.com>
2024-11-06 18:51:07 -0500

    Update on resume

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -6,7 +6,7 @@ function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
       <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        James <strong className="purple">Nguyen</strong>
+        
       </h1>
       
     </Row>
